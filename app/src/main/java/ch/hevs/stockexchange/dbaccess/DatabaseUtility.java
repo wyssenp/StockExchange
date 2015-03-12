3ed9d09b1aa7d21108650621a6b1a84032bbf69a
maniac1989 <pierry_wyssen@hotmail.com>
2015-03-12 11:41:46 +0100

    Added creation of all tables (SQL)

--- app/src/main/java/ch/hevs/stockexchange/dbaccess/DatabaseUtility.java
+++ app/src/main/java/ch/hevs/stockexchange/dbaccess/DatabaseUtility.java
@@ -16,6 +16,41 @@ public class DatabaseUtility extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "StockExchange.db";
     private static final String DB_PATH = "/mnt/sdcard/" ;
     private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_STOCK = "Stock";
+    private static final String TABLE_BROKER = "Broker";
+    private static final String TABLE_STOCKMARKET = "Stockmarket";
+    private static final String TABLE_PORTFOLIO = "Portfolio";
+
+    private static final String TABLE_STOCKMARKET_CREATE = "CREATE TABLE IF NOT EXISTS " + TABLE_STOCKMARKET +
+            "(stockMarketId INTEGER PRIMARY KEY NOT NULL,"+
+            "Symbol TEXT NOT NULL,"+
+            "Name TEXT NOT NULL,"+
+            "Currency TEXT NOT NULL);";
+
+    private static final String TABLE_STOCK_CREATE = "CREATE TABLE IF NOT EXISTS " + TABLE_STOCK +
+            "(stockId INTEGER PRIMARY KEY NOT NULL,"+
+            "Symbol TEXT NOT NULL,"+
+            "Name TEXT NOT NULL,"+
+            "Sector TEXT NOT NULL,"+
+            "StockMarket INTEGER NOT NULL,"+
+            "StockValue DOUBLE NOT NULL,"+
+            "FOREIGN KEY(StockMarket) REFERENCES " + TABLE_STOCKMARKET + "(stockMarketId));";
+
+    private static final String TABLE_BROKER_CREATE = "CREATE TABLE IF NOT EXISTS " + TABLE_BROKER +
+            "(brokerId INTEGER PRIMARY KEY NOT NULL,"+
+            "Name TEXT NOT NULL,"+
+            "BankType TEXT NOT NULL,"+
+            "SecuritiesDealerType TEXT NOT NULL);";
+
+    private static final String TABLE_PORTFOLIO_CREATE = "CREATE TABLE IF NOT EXISTS " + TABLE_PORTFOLIO +
+            "(portfolioId INTEGER PRIMARY KEY NOT NULL,"+
+            "Stock INTEGER NOT NULL,"+
+            "Broker INTEGER NOT NULL,"+
+            "Market INTEGER NOT NULL,"+
+            "FOREIGN KEY(Stock) REFERENCES " + TABLE_STOCK + "(stockId),"+
+            "FOREIGN KEY(Broker) REFERENCES " + TABLE_BROKER + "(brokerId),"+
+            "FOREIGN KEY(Market) REFERENCES " + TABLE_STOCKMARKET + "(stockMarketId));";
+
 
     public DatabaseUtility(Context context) {
         super(context, DB_PATH + DATABASE_NAME, null, DATABASE_VERSION);
@@ -23,11 +58,18 @@ public class DatabaseUtility extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(TABLE_STOCKMARKET_CREATE);
+        db.execSQL(TABLE_STOCK_CREATE);
+        db.execSQL(TABLE_BROKER_CREATE);
+        db.execSQL(TABLE_PORTFOLIO_CREATE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_STOCK);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_BROKER);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_STOCKMARKET);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_PORTFOLIO);
+        onCreate(db);
     }
 }
