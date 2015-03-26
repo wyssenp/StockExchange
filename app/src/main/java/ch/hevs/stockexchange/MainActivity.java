e9ece2e065c58f3afa2d6a41804792b5ee721aab
wyssenp <pierry_wyssen@hotmail.com>
2015-03-26 18:29:41 +0100

    Added permission to use Internet Exchange rates get updated everytime the user launches the app

--- app/src/main/java/ch/hevs/stockexchange/MainActivity.java
+++ app/src/main/java/ch/hevs/stockexchange/MainActivity.java
@@ -2,12 +2,27 @@ package ch.hevs.stockexchange;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+
+import ch.hevs.stockexchange.dbaccess.DatabaseAccessObject;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -16,6 +31,7 @@ public class MainActivity extends ActionBarActivity {
     private Button stockExchange;
     private Button stockManagement;
     private Context ctx;
+    private DatabaseAccessObject datasource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +43,8 @@ public class MainActivity extends ActionBarActivity {
         stockManagement = (Button) findViewById(R.id.btn_stockMgmt);
 
         ctx = getApplicationContext();
+
+        new DownloadTask().execute();
 
         myPortfolio.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,6 +71,48 @@ public class MainActivity extends ActionBarActivity {
         });
     }
 
+    private class DownloadTask extends AsyncTask<Void, Integer, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                URL url = new URL("http://download.finance.yahoo.com/d/quotes.csv?s=USDEUR=x,CHFUSD=x,CHFEUR=x&f=l1");
+                URLConnection connection = url.openConnection();
+                connection.connect();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+
+                List<Double> exchangeRates = new ArrayList<>();
+
+                String line;
+                while((line = reader.readLine()) != null) {
+                    String[] rowData = line.split(",");
+                    exchangeRates.add(Double.parseDouble(rowData[0]));
+                }
+
+                reader.close();
+
+                //Data available at that point: USDEUR,CHFUSD,CHFEUR
+                double eur2usd = 1/exchangeRates.get(0);
+                double usd2chf = 1/exchangeRates.get(1);
+                double eur2chf = 1/exchangeRates.get(2);
+
+                datasource = new DatabaseAccessObject(ctx);
+                datasource.open();
+
+                //double chf_eur, double chf_usd, double eur_chf, double eur_usd, double usd_chf, double usd_eur
+                datasource.updateExchangeRates(exchangeRates.get(2),exchangeRates.get(1),eur2chf,eur2usd,usd2chf,exchangeRates.get(0));
+
+                datasource.close();
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
